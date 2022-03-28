9ec82d38d1835d42fddc4f2ab6d423829a5365e3
Florès, Maoro - Student Rijn IJssel <00303196@rijnijssel.nl>
2022-03-28 13:45:25 +0200

    Initial commit

---
+++ includes/about.inc.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<section class="about">
+    <div class="about_container">
+        <h1>About page</h1>
+
+        <h3>Something about the project</h3>
+
+        <div class="about_me">
+            <h3>Something about me</h3>
+            <div class="about_me_inner"
+        </div>
+    </div>
+</section>
+
