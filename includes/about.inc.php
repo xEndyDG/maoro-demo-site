59b53c9f051750146229b10fab2c53a2ebc36431
Florès, Maoro - Student Rijn IJssel <00303196@rijnijssel.nl>
2022-03-29 14:14:15 +0200

    Layout complete

--- includes/about.inc.php
+++ includes/about.inc.php
@@ -3,77 +3,154 @@
 <section class="about">
     <div class="about_container">
         <div class="about__main">
-            <div class="about__me__info">
-                foto: <img class="img" src="images/pfp.jpg" style="max-height: 150px; max-width: 150px">
-                <h2>persoonlijke informatie:</h2>
-                <ul>
-                    <li>Website</li>
-                    <li>Podcast</li>
-                    <li>linkedin</li>
-                    <li>vierjaardag</li>
-                    <li>nummer</li>
-                </ul>
-            </div>
-            <div class="about__me__skills">
-                <h2>Competenties:</h2>
-                <ul>
-                    <li>
-                        Coderen
-                        <i>Taal 1</i>
-                        <i>Taal 1</i>
-                        <i>Taal 1</i>
-                        <i>Taal 1</i>
-                    </li>
-                    <li>Creatief</li>
-                    <li>Management</li>
-                    <li>Flexiebel</li>
-                </ul>
-            </div>
-            <div class="about__me__title">
-                <h1>About page</h1>
-                <p><i>Een temporary portfolio voor mezelf</i></p>
-            </div>
-            <div class="about__me__main">
-                <h2>Projecten</h2>
-                <span>
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Facere officiis quaerat quasi similique.
-                    Blanditiis cum dicta dolor dolorem excepturi expedita, itaque iusto laudantium,
-                    maxime possimus quis repellat, sequi ut voluptate!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    <h3>Amet,</h3>
-                    cumque dolorem fugit ipsa odio porro quaerat vero!
-                    Ab consectetur deleniti impedit ipsum, laborum quam quidem rerum sint, sunt totam voluptatem.
-                    <ul>
-                        <li>lorem impus</li>
-                        <li>lorem impus</li>
-                        <li>lorem impus</li>
-                    </ul>
-                </span>
-                <h2>Opleidingen</h2>
-                Margin top zoveel px
-                <h3>Opleiding</h3>
-                <p>Iets over de opleiding of het bewijs</p>
-                <h3>Opleiding</h3>
-                <p>Iets over de opleiding of het bewijs</p>
-                <h3>Opleiding</h3>
-                <p>Iets over de opleiding of het bewijs</p>
+            <aside class="sidebar">
+                <div class="persoonlijke_informatie">
+                    <div class="persoonlijke_informatie_image">
+                        <img class="img" src="images/pfp.jpg">
+                    </div>
+                    <div class="informatie__content">
+                        <div class="informatie-content-title">
+                            <h2>persoonlijke informatie:</h2>
+                        </div>
+                        <div class="informatie-content-item">
+                            <a href="https://projectdg.eu/">Website</a>
+                        </div>
+                        <div class="informatie-content-item">
+                            <a href="https://open.spotify.com/show/0pV3t8VIUDbV5xCj2U76HH?si=b4af9e416157482b">
+                                Podcast
+                            </a>
+                        </div>
+                        <div class="informatie-content-item">
+                            <a href="">Linkedin</a>
+                        </div>
+                        <div class="informatie-content-item">
+                            Nummer
+                        </div>
+                    </div>
 
-                <h2>Talen</h2>
-                <h3>Taal</h3>
-                <p>Ervaring met de taal</p>
-                <h3>Taal</h3>
-                <p>Ervaring met de taal</p>
-                <h3>Taal</h3>
-                <p>Ervaring met de taal</p>
+                </div>
+                <div class="competenties">
+                    <div class="competenties-item">
+                        <h2>Competenties:</h2>
+                    </div>
+                    <div class="competenties-item">
+                        <h3>Coderen</h3>
+                        <p>Taal1</p>
+                        <p>Taal2</p>
+                        <p>Taal3</p>
+                        <p>
+                            <Taal4></Taal4>
+                        </p>
+                    </div>
+                    <div class="competenties-item">
+                        <h2>Creatief</h2>
+                        <p>Hier iets over de creatieve aspecten van
+                            of dit portfolie bestand of
+                            Of mijn creativiteit.
+                        </p>
+                    </div>
+                    <div class="competenties-item">
+                        <h2>Management</h2>
+                        <p>
+                            Iets over de management die ik vericht
+                        </p>
+                    </div>
+                    <div class="competenties-item">
+                        <h2>Flexiebel</h2>
+                        <p>
+                            Hier iets over de flexiebiliteit  leer spellen LOL
+                        </p>
+                    </div>
+                </div>
+            </aside>
+            <div class="main__content">
+                <div class="title">
+                    <h1>my portfolio</h1>
+                    <p><i>Een temporary portfolio voor en over mezelf</i></p>
+                </div>
+                <div class="main-content-box">
+                    <div class="main-item-1">
+                        <h2>Projecten</h2>
+                        <h3>Sub title</h3>
+                        <span>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Facere officiis quaerat quasi similique.
+                        Blanditiis cum dicta dolor dolorem excepturi expedita, itaque iusto laudantium,
+                        maxime possimus quis repellat, sequi ut voluptate!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        <h3>Amet,</h3>
+                        cumque dolorem fugit ipsa odio porro quaerat vero!
+                        Ab consectetur deleniti impedit ipsum, laborum quam quidem rerum sint, sunt totam voluptatem.
 
-                <h2>interesses</h2>
-                <h3>Interesse</h3>
-                <p>Iets over de interesse / de hobby</p>
-                <h3>Interesse</h3>
-                <p>Iets over de interesse / de hobby</p>
-                <h3>Interesse</h3>
-                <p>Iets over de interesse / de hobby</p>
+                        <h3>Sub title</h3>
+                        <span>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Facere officiis quaerat quasi similique.
+                        Blanditiis cum dicta dolor dolorem excepturi expedita, itaque iusto laudantium,
+                        maxime possimus quis repellat, sequi ut voluptate!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        <h3>Amet,</h3>
+                        cumque dolorem fugit ipsa odio porro quaerat vero!
+                        Ab consectetur deleniti impedit ipsum, laborum quam quidem rerum sint, sunt totam voluptatem.
+
+                        <h3>Sub title</h3>
+                        <span>
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Facere officiis quaerat quasi similique.
+                        Blanditiis cum dicta dolor dolorem excepturi expedita, itaque iusto laudantium,
+                        maxime possimus quis repellat, sequi ut voluptate!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        <h3>Amet,</h3>
+                        cumque dolorem fugit ipsa odio porro quaerat vero!
+                        Ab consectetur deleniti impedit ipsum, laborum quam quidem rerum sint, sunt totam voluptatem.
+                        <ul>
+                            <li>lorem impus</li>
+                            <li>lorem impus</li>
+                            <li>lorem impus</li>
+                        </ul>
+                        </span>
+                    </div>
+                    <div class="main-item-2">
+                        <h2>Opleidingen</h2>
+                        Margin top zoveel px
+                        <h3>Opleiding</h3>
+                        <p>Iets over de opleiding of het bewijs</p>
+                        <h3>Opleiding</h3>
+                        <p>Iets over de opleiding of het bewijs</p>
+                        <h3>Opleiding</h3>
+                        <p>Iets over de opleiding of het bewijs</p>
+                    </div>
+                    <div class="main-item-3">
+                        <h2>Talen</h2>
+                        <h3>Taal</h3>
+                        <p>Ervaring met de taal</p>
+                        <h3>Taal</h3>
+                        <p>Ervaring met de taal</p>
+                        <h3>Taal</h3>
+                        <p>Ervaring met de taal</p>
+                    </div>
+                    <div class="main-item-4">
+                        <div class="main-title">
+                            <h2>interesses</h2>
+                        </div>
+                        <div class="main-interesse">
+                            <h3>Interesse</h3>
+                            <p>Iets over de interesse / de hobby</p>
+                        </div>
+                        <div class="main-interesse">
+                            <h3>Interesse</h3>
+                            <p>Iets over de interesse / de hobby</p>
+                        </div>
+                        <div class="main-interesse">
+                            <h3>Interesse</h3>
+                            <p>Iets over de interesse / de hobby</p>
+                        </div>
+                        <div class="main-interesse">
+                            <h3>Interesse</h3>
+                            <p>Iets over de interesse / de hobby</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
