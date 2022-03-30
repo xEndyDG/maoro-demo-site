21bdb027eb576e18c824369076cb45b242a6ef13
maoro <maoro@booomdigital.nl>
2022-03-30 22:13:03 +0200

    To do list en footer fix

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,14 @@
 <section class="footer">
-    <div class="footer__container">
-        <p><i>Hier iets toekomstelijks</i></p>
+    <div class="footer-container">
+        <div class="footer-contact">
+            <h2><a href="https://maoro.nl">Website</a></h2>
+            <h2><a href="#">Linkedin</a></h2>
+            <h2><a href="https://github.com/xEndyDG">Github</a></h2>
+            <h2><a href="">Email</a></h2>
+            <h2><a href="https://open.spotify.com/show/0pV3t8VIUDbV5xCj2U76HH?si=fa56298089fe4248">Podcast</a></h2>
+        </div>
+        <div class="footer-extra">
+            <p><i>Hier iets toekomstelijks copyright ofzo. </i></p>
+        </div>
     </div>
 </section>
