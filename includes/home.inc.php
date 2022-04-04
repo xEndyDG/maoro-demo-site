4125038a7fcf2a7e1a65fd549214816678b286a9
maoro <maoro@booomdigital.nl>
2022-04-04 22:11:08 +0200

    Ideen Footer fix margin/ padding

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,50 +1,120 @@
 <section class="home">
     <div class="home-container">
-        <div class="home-title">
-            <h1>Home page</h1>
-        </div>
-        <div class="home-content">
-            <div class="home-content-blok">
-                <h2>Hier iets!</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
-                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
-                    suscipit velit.
-                </p>
-            </div>
-            <div class="home-content-blok">
-                <h2>Hier iets!</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
-                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
-                    suscipit velit.
-                </p>
-            </div>
-            <div class="home-content-blok">
-                <h2>Hier iets!</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
-                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
-                    suscipit velit.
-                </p>
-            </div>
-            <div class="home-content-blok">
-                <h2>Hier iets!</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
-                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
-                    suscipit velit.
-                </p>
-            </div>
-            <div class="home-content-blok">
-                <h2>Hier iets!</h2>
-                <div class="folder">
-                    <div class="folder-title">
-                        Project hub
-                    </div>
 
+        <!--        title-->
+        <div class="home-title">
+<!--            Iets met de $darkgreen hier-->
+
+<!--            Ideeen:-->
+<!--            Bloemen die uit de header komen-->
+<!--            Bloemen die uit de bovenkant van de pagina groeien (roses???)-->
+
+
+<!--            file name: ander font-->
+            <h1>File name: maoro.nl</h1>
+        </div>
+
+        <!--        content-->
+
+        <div class="home-content">
+            <div class="home-content-blok big-text">
+                <!--                dit wordt steeds opnieuw getyped en verwijderd op een loop-->
+                <h2>Index.html</h2>
+                <div class="table-of-content">
+                    <div class="table-of-content-item">
+                        <p>01</p>
+                        <p>about me</p>
+                    </div>
+                    <div class="table-of-content-item">
+                        <p>02</p>
+                        <p>temp</p>
+                    </div>
+                    <div class="table-of-content-item">
+                        <p>03</p>
+                        <p>temp</p>
+                    </div>
+                    <div class="table-of-content-item">
+                        <p>04</p>
+                        <p>temp</p>
+                    </div>
+                    <div class="table-of-content-item">
+                        <p>05</p>
+                        <p>temp</p>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="home-content">
+            every other switch sides
+
+        </div>
+        <div class="home-content">
+
+            <!--            Een neon slang wat door de code blok elementen heen loopt op een timer. Zodra het 100% berijkt heeft kippert-->
+            <!--            het en gaat het uit. Dan begint het weer opnieuw.-->
+
+            <!--            Een Kerstboom / boom met de feestelijke primary kleuren rood en groen-->
+
+            <!--            Neon cards die een flow effect krijgen wanneer je over ze heen hovert wat 2 kleuren heeft die als een-->
+            <!--            grandient rond de kaart border heen bewegen-->
+
+            <!--            objecten waarover de kopjes gaan maar dan in neon / 3d maar dan hollow met neon lijntjes.
+                            Bijv een kopje gaat over een telefoon dan een 3d telefoon met neon outline -->
+            <!--            mogelijke 3d neon objecten:-->
+            <!--            muts-->
+            <!--            thee kopje-->
+            <!--            telefoon-->
+            <!--            kaas-->
+            <!--            sport equipment-->
+
+            <!--            elk content blok een ander thema-->
+            <!--            bijv: pac man, dino's,-->
+
+
+            <!--            elk content blok gaat over iets wat me interesseert-->
+            <div class="home-content-blok">
+                <h2>Hier iets!</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
+                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
+                    suscipit velit.
+                </p>
+            </div>
+            <div class="home-content-blok">
+                <h2>Hier iets!</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
+                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
+                    suscipit velit.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
+                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
+                    suscipit velit.
+                </p>
+            </div>
+            <div class="home-content-blok">
+                <h2>Hier iets!</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
+                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
+                    suscipit velit.
+                </p>
+            </div>
+            <div class="home-content-blok">
+                <h2>Hier iets!</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, atque, delectus deleniti dicta eius
+                    fuga ipsam iure, molestiae odio omnis praesentium quis unde vero. Eius eligendi enim eveniet
+                    suscipit velit.
+                </p>
+            </div>
+            <!--            <div class="home-content-blok">-->
+            <!--                <h2>Hier iets!</h2>-->
+            <!--                <div class="folder">-->
+            <!--                    <div class="folder-title">-->
+            <!--                        Project hub-->
+            <!--                    </div>-->
+            <!--                </div>-->
+            <!--            </div>-->
         </div>
     </div>
 </section>
